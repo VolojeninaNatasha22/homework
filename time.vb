d983273ef8ffa92d4457912af6ccab61510217ab
VolojeninaNatasha22 <122751999+VolojeninaNatasha22@users.noreply.github.com>
2023-03-06 03:34:10 +0300

    Add files via upload

---
+++ time.vb
@@ -0,0 +1,17 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim seconds, hours, minutes, remainingSeconds As Integer
+
+        Console.Write("введите время в секундах: ")
+        seconds = Console.ReadLine
+
+        hours = seconds \ 3600
+        remainingSeconds = seconds Mod 3600
+        minutes = remainingSeconds \ 60
+        remainingSeconds = remainingSeconds Mod 60
+
+        Console.Write("Time: " & hours & " hours, " & minutes & " minutes, " & remainingSeconds & " seconds")
+    End Sub
+End Module
